0662eced1df58f940c4fa3d0848f13c26d28e1cc
Douglas-prog23 <131540385+Douglas-prog23@users.noreply.github.com>
2023-11-16 07:04:57 -0600

    hora

--- lib/helper/my_date_util.dart
+++ lib/helper/my_date_util.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyDateUtil {
   // for getting formatted time from milliSecondsSinceEpochs String
+  //para obtener la hora formateada de milliSecondsSinceEpochs String
   static String getFormattedTime(
       {required BuildContext context, required String time}) {
     final date = DateTime.fromMillisecondsSinceEpoch(int.parse(time));
