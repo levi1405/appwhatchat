dc07da826b507bc6146a3a41081e87c6bd7261fe
adonay2003 <gerson.melara22@itca.edu.sv>
2023-11-15 21:00:30 -0600

    agregue comentario en el main.dart parte 2 para documentar el codigo

--- lib/main.dart
+++ lib/main.dart
@@ -32,29 +32,57 @@ void main() {
 }
 
 
+// Clase 'MyApp' que representa la aplicación Flutter.
 class MyApp extends StatelessWidget {
+  // Constructor constante para la clase 'MyApp'.
   const MyApp({super.key});
 
+  // Método 'build' que construye la interfaz de usuario de la aplicación.
   @override
   Widget build(BuildContext context) {
+    // Devuelve un objeto 'MaterialApp' que define la estructura y el tema de la aplicación.
     return MaterialApp(
+        // Título de la aplicación.
         title: 'What Chat',
+
+        // Oculta el banner de depuración en la esquina superior derecha.
         debugShowCheckedModeBanner: false,
+
+        // Tema de la aplicación que define la apariencia general de la interfaz de usuario.
         theme: ThemeData(
+            // Tema de la barra de aplicación.
             appBarTheme: const AppBarTheme(
+          // Centra el título en la barra de aplicación.
           centerTitle: true,
+
+          // Elevación de la barra de aplicación (sombra).
           elevation: 1,
+
+          // Configuración del tema de los iconos en la barra de aplicación.
           iconTheme: IconThemeData(color: Colors.black),
+
+          // Estilo del texto del título en la barra de aplicación.
           titleTextStyle: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.normal, fontSize: 19),
+              color: Colors.black, 
+              fontWeight: FontWeight.normal, 
+              fontSize: 19),
+
+          // Color de fondo de la barra de aplicación.
           backgroundColor: Colors.white,
         )),
+
+        // Página de inicio de la aplicación, en este caso, se establece como la pantalla de presentación 'SplashScreen'.
         home: const SplashScreen());
   }
 }
 
+// Función privada '_initializeFirebase' para inicializar Firebase de forma asíncrona.
 _initializeFirebase() async {
+  // Espera a que la inicialización de Firebase se complete.
+  // 'options' se utiliza para proporcionar opciones de configuración adicionales, en este caso, utiliza las opciones predeterminadas para la plataforma actual.
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+
 
   // var result = await FlutterNotificationChannel.registerNotificationChannel(
   //     description: 'Para mostrar notificaciones de mensajes',
